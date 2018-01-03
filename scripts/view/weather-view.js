09c54d105d724df8387ab9aaee84fd3a78ecf4cd
Jordan <JordanLesaca@Gmail.Com>
2018-01-03 11:24:55 -0800

    button stuff

--- scripts/view/weather-view.js
+++ scripts/view/weather-view.js
@@ -6,23 +6,19 @@ var app = app || {};
     weatherView.initLoginPage = err => {
       $('.container').hide();
       $('.login').show();
-     
       $('.form').on('submit', callback);
     };
-
-    
     
     weatherView.initFormPage = callback => {
       $('.container').hide();
       $('.input').show();
-      
       $('.form-two').on('submit', callback);
     };
     
-    weatherView.initOutputPage = callback => {
-      $('.container').hide();
-      $('.output').show();
-    };
+    // weatherView.initOutputPage = function () {
+    //   $('.container').hide();
+    //   $('.output').show();
+    // };
     
     weatherView.initLoginPage();
     weatherView.initFormPage();
