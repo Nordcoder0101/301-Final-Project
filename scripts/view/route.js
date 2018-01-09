56ac996d0269e433a3ccb5aa4f95561417063fde
sundownspecial <je.mendoza@live.com>
2018-01-09 10:06:11 -0800

    testing code to make pagejs work with git hub pages

--- scripts/view/route.js
+++ scripts/view/route.js
@@ -1,13 +1,13 @@
 'use strict';
 
-// page('/', () => app.Book.fetchAll(app.bookView.initIndexPage))
-// page('/weather/input', ctx => app.Weather.fetchWeather(ctx.params.id, app.weatherView.initOutputPage))
+if (window.location.pathname !== '/'){
+    page.base('/301-Final-Project');
+} 
 
 page('/', app.weatherView.initHomePage);
 page('/user', app.weatherView.initFormPage);
 page('/output', app.weatherView.initOutputPage);
 page('/about', app.weatherView.initAboutPage);
 page('/login', app.weatherView.initLoginPage);
-// page('*', '/');
 page('/register', app.weatherView.initRegistration);
 page();
