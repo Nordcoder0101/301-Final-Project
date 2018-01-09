26341cd07bdd86662022b6f4ff259f07fef5f7ca
sundownspecial <je.mendoza@live.com>
2018-01-09 10:33:21 -0800

    page js works in github pages now just cleaned up the dead code

--- scripts/view/route.js
+++ scripts/view/route.js
@@ -1,20 +1,7 @@
 'use strict';
 
-// if (window.location.pathname !== '/'){
-//     page.base('/301-Final-Project');
-// } 
-
 page.base('/301-Final-Project');
 
-// use this for local testing
-// page('/', app.weatherView.initHomePage);
-// page('/user', app.weatherView.initFormPage);
-// page('/output', app.weatherView.initOutputPage);
-// page('/about', app.weatherView.initAboutPage);
-// page('/login', app.weatherView.initLoginPage);
-// page('/register', app.weatherView.initRegistration);
-
-//use this for github pages deployment
 page('/', app.weatherView.initHomePage);
 page('/user', app.weatherView.initFormPage);
 page('/output', app.weatherView.initOutputPage);
